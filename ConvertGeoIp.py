ef2a489772b479a42db35b51097a0281da494927
Donghyun <mpolio2@kyonggi.ac.kr>
2023-03-15 19:31:02 +0900

    edit name

--- ConvertGeoIp.py
+++ ConvertGeoIp.py
@@ -3,4 +3,4 @@ import pandas as pd
 geoip = pd.read_csv('/root/GeoLite2xtables/GeoIP-legacy.csv')
 geoip = geoip.iloc[:, [0,1,4]]
 
-geoip.to_csv('GeoIP-legacy2.csv', index=None)
+geoip.to_csv('GeoIP-legacy.csv', index=None)
